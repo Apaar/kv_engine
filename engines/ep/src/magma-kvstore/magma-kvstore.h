673dbcde4af500bfd9c058ba4681dcac8d442467
Sarath Lakshman <sarathlakshman@slynux.com>
2020-01-17 10:21:01 +0530

    magma-kvstore: Reuse stringstream in compaction callback
    Perf profile showed that std::stringstream object construction/destruction
    is consuming signifiant cpu even when the object is not used for string
    allocation. This causes spikes in write queue when compaction is
    performed. Implemented stringstream object reuse per compaction instance
    to amortize the cost of object construction/destruction.
    
    Perf profile:
    -   21.65%     0.00%  [unknown]                     [.] 0000000000000000
       - 0
          + 14.41% 0x6
          + 2.75% magma::DocSeqGCContext::Transform
            1.67% std::locale::operator=
            0.69% std::locale::~locale
    
    Change-Id: I697f8a4ce8b154e121bfa1f2ce5ca05a8a69a0b1

--- engines/ep/src/magma-kvstore/magma-kvstore.h
+++ engines/ep/src/magma-kvstore/magma-kvstore.h
@@ -580,6 +580,7 @@ private:
         }
         MagmaKVStore& magmaKVStore;
         bool initialized{false};
+        std::stringstream itemKeyBuf;
         std::shared_ptr<MagmaCompactionCtx> magmaCompactionCtx;
         compaction_ctx* ctx{nullptr};
         MagmaKVHandle kvHandle;
